6d52d58a640f65ad129a850a3df33eb1131994a3
毛天楷 <110109827+Sphered0166@users.noreply.github.com>
2024-12-20 18:39:16 -0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let score = 0;
-const scoreDisplay = document.getElementById('score');
-const gameButton = document.getElementById('game-button');
-
-gameButton.addEventListener('click', () => {
-    score += 1;
-    scoreDisplay.textContent = score;
-});
